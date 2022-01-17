a1c73a5389622f19cc0e7ac43bb745b88c8328e1
Grzegorz Tomicki <gtomik@gmail.com>
2022-01-17 12:30:54 +0100

    moving babel config to rollup, add IE version lib

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,148 @@
-import serve from 'rollup-plugin-serve';
-import babel from '@rollup/plugin-babel';
-import livereload from 'rollup-plugin-livereload';
+import { babel } from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import cleanup from "rollup-plugin-cleanup";
+
 import pkg from "./package.json";
 
 const { PRODUCTION } = process.env;
+const input = "sources/js/index.js";
 
-const plugins = () => {
-  return [
-    babel({
-      exclude: 'node_modules/**',
-      babelHelpers: 'bundled'
-    }),
-    PRODUCTION && terser(),
-    !PRODUCTION && serve({ open: true, contentBase: 'docs' }),
-    !PRODUCTION && livereload()
-  ]
+const targets = {
+  targets: {
+    browsers: ["defaults", "not IE 11", "maintained node versions"],
+  },
 };
+
+const targetsIE = {
+  targets: {
+    browsers: [">0.2%", "not dead", "not op_mini all"],
+  },
+};
+
+const compressTarget = (type, console = true) => {
+  return {
+    mangle: type ? false : true,
+    output: {
+      beautify: type,
+    },
+    compress: { drop_console: console, drop_debugger: console },
+  };
+};
+
+const pluginsConfig = (target) => [
+  babel({
+    babelHelpers: "bundled",
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          // debug: true,
+          // useBuiltIns: 'usage',
+          useBuiltIns: "entry",
+          corejs: 3,
+          loose: true,
+          ...target,
+        },
+      ],
+    ],
+  }),
+  cleanup(),
+];
 
 export default [
   {
-    input: 'sources/js/index.js',
-    output: {
-      file: pkg.main,
-      format: 'iife',
-      name: 'SpeedDial',
-      sourcemap: !PRODUCTION
-    },
-    plugins: plugins(),
-  },
-  {
-    input: 'sources/js/index.js',
+    input,
+    plugins: pluginsConfig(targets),
     watch: false,
     output: {
-      file: pkg.browser,
-      format: 'umd',
-      name: 'SpeedDial'
+      name: "SpeedDial",
+      format: "iife",
+      file: pkg.main,
+      sourcemap: true,
+      plugins: [terser(compressTarget(true))],
     },
-    plugins: plugins()
-  }
+  },
+  {
+    input,
+    plugins: pluginsConfig(targets),
+    watch: false,
+    output: {
+      name: "SpeedDial",
+      format: "iife",
+      sourcemap: true,
+      file: "dist/js/speed-dial.min.js",
+      plugins: [terser(compressTarget(false))],
+    },
+  },
+  {
+    input,
+    plugins: pluginsConfig(targets),
+    output: {
+      name: "SpeedDial",
+      format: "iife",
+      sourcemap: true,
+      file: "docs/speed-dial.min.js",
+      plugins: [
+        terser(compressTarget(true, false)),
+        !PRODUCTION && serve({ open: true, contentBase: ["docs"] }),
+        !PRODUCTION && livereload(),
+      ],
+    },
+  },
+  {
+    input,
+    plugins: pluginsConfig(targets),
+    watch: false,
+    output: [
+      {
+        name: "SpeedDial",
+        format: "umd",
+        sourcemap: true,
+        file: "dist/js/speed-dial.umd.js",
+        plugins: [terser(compressTarget(true))],
+      },
+      {
+        name: "SpeedDial",
+        format: "umd",
+        sourcemap: false,
+        file: "dist/js/speed-dial.umd.min.js",
+        plugins: [terser(compressTarget(false))],
+      },
+    ],
+  },
+  {
+    input,
+    plugins: pluginsConfig(targets),
+    watch: false,
+    output: [
+      {
+        name: "SpeedDial",
+        format: "es",
+        sourcemap: true,
+        file: "dist/js/speed-dial.esm.js",
+        plugins: [terser(compressTarget(true))],
+      },
+      {
+        name: "SpeedDial",
+        format: "es",
+        sourcemap: false,
+        file: "dist/js/speed-dial.esm.min.js",
+        plugins: [terser(compressTarget(false))],
+      },
+    ],
+  },
+  {
+    input,
+    plugins: pluginsConfig(targetsIE),
+    watch: false,
+    output: {
+      name: "SpeedDial",
+      format: "iife",
+      sourcemap: false,
+      file: "dist/js/speed-dial.ie.min.js",
+      plugins: [terser(compressTarget(false))],
+    },
+  },
 ];
