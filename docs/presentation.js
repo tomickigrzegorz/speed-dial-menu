804f008e66b41134ebeead400d9eb714aa8af431
Grzegorz Tomicki <gtomik@gmail.com>
2022-01-17 12:34:27 +0100

    chore: update example

--- docs/presentation.js
+++ docs/presentation.js
@@ -13,77 +13,276 @@ const githubConrner = `
 </svg></a>
 `;
 
-// better presentation
-const template = `
-<h3>CHANGE DISPLAY POSITION</h3>
-<label class="radio">
-  <input type="radio" name="type" value="1" checked>
-  <span>bottom-right + top</span>
-</label>
-<label class="radio">
-  <input type="radio" name="type" value="2">
-  <span>bottom-left + right</span>
-</label>
-<label class="radio">
-  <input type="radio" name="type" value="3">
-  <span>top-right + bottom</span>
-</label>
-<label class="radio">
-  <input type="radio" name="type" value="4">
-  <span>top-left + bottom</span>
-</label>
-`;
+const config = [
+  {
+    id: 1,
+    type: "label",
+    input: "checkbox",
+    title: "top left",
+    position: "top-left",
+    "data-tippy-content": "show top left",
+    svg: "arrow-top-left",
+    default: false,
+    set: [1, 6, 8],
+  },
+  {
+    id: 2,
+    type: "label",
+    input: "radio",
+    title: "top",
+    direction: "top",
+    "data-tippy-content": "show top",
+    svg: "arrow-top",
+    default: true,
+    set: [2, 7, 9],
+  },
+  {
+    id: 3,
+    type: "label",
+    input: "checkbox",
+    title: "top right",
+    position: "top-right",
+    "data-tippy-content": "show top right",
+    svg: "arrow-top-right",
+    default: false,
+    set: [3, 4, 8],
+  },
+  {
+    id: 4,
+    type: "label",
+    input: "radio",
+    title: "left",
+    direction: "left",
+    "data-tippy-content": "show left",
+    svg: "arrow-left",
+    default: false,
+    set: [4, 3, 9],
+  },
+  {
+    id: 5,
+    type: "div",
+    title: "reset",
+    position: "bottom-right",
+    direction: "top",
+    "data-tippy-content": "reset",
+    set: [5, 4, 9],
+  },
+  {
+    id: 6,
+    type: "label",
+    input: "radio",
+    title: "right",
+    direction: "right",
+    "data-tippy-content": "show right",
+    svg: "arrow-right",
+    default: false,
+    set: [6, 1, 7],
+  },
+  {
+    id: 7,
+    type: "label",
+    input: "checkbox",
+    title: "bottom left",
+    position: "bottom-left",
+    "data-tippy-content": "show bottom left",
+    svg: "arrow-bottom-left",
+    default: false,
+    set: [7, 2, 6],
+  },
+  {
+    id: 8,
+    type: "label",
+    title: "bottom",
+    input: "radio",
+    direction: "bottom",
+    "data-tippy-content": "show bottom",
+    svg: "arrow-bottom",
+    default: false,
+    set: [8, 1, 3],
+  },
+  {
+    id: 9,
+    type: "label",
+    input: "checkbox",
+    title: "bottom right",
+    position: "bottom-right",
+    "data-tippy-content": "show bottom right",
+    svg: "arrow-bottom-right",
+    default: true,
+    set: [9, 2, 4],
+  },
+];
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
+  document.body.insertAdjacentHTML("beforeend", githubConrner);
 
-  document.body.insertAdjacentHTML('beforeend', githubConrner);
+  const h1 = document.createElement("h1");
+  h1.textContent = "change display position and direction";
 
-  const place = document.createElement('div');
-  place.insertAdjacentHTML('beforeend', template);
+  const centerPlace = document.createElement("div");
+  centerPlace.classList.add("center-place");
 
-  const control = document.getElementById('control');
-  control.insertAdjacentElement('beforeend', place);
+  const control = document.getElementById("control");
+  control.insertAdjacentElement("beforeend", centerPlace);
 
-  const radioElements = document.querySelectorAll('[name="type"]');
-  for (let i = 0; i < radioElements.length; i++) {
-    radioElements[i].addEventListener('click', function () {
-      const speedDial = document.querySelector('.speed-dial');
-      const speedDialTop = document.querySelector('.speed-dial__top');
+  control.insertAdjacentElement("beforebegin", h1);
 
-      speedDial.parentNode.removeChild(speedDial);
-      speedDialTop ? speedDialTop.parentNode.removeChild(speedDialTop) : '';
+  function createElement(item) {
+    const type = item.type === "label" ? true : false;
 
-      let newPosition;
-      let newDairection;
-      switch (this.value) {
-        case '1':
-          newPosition = 'bottom-right';
-          newDairection = 'top';
-          break;
-        case '2':
-          newPosition = 'bottom-left';
-          newDairection = 'right';
-          break;
-        case '3':
-          newPosition = 'top-right';
-          newDairection = 'bottom';
-          break;
-        case '4':
-          newPosition = 'top-left';
-          newDairection = 'bottom';
-        default:
-          break;
+    const el = document.createElement(type ? "label" : "div");
+    el.setAttribute("data-id", item.id);
+
+    if (item.position) {
+      el.setAttribute("data-position", item.position);
+    } else {
+      el.setAttribute("data-direction", item.direction);
+    }
+
+    el.classList.add(type ? "form-control" : "reset");
+    el.title = item.title;
+    el.setAttribute("data-tippy-content", item["data-tippy-content"]);
+
+    if (type) {
+      let checkedCheck;
+      if (item.id === 2 || item.id === 4 || item.id === 9) {
+        el.classList.add("active");
+      } else if (item.input === "radio") {
+        el.classList.add("disabled");
       }
+
+      const typeCheckOrRadio = item.input === "radio" ? "radio" : "checkbox";
+
+      el.innerHTML = `<input type="${typeCheckOrRadio}" name="type" value="${item.id}"><svg><use xlink:href="#${item.svg}"></use></svg>`;
+
+      if (item.id === 2 || item.id === 9) {
+        el.firstElementChild.checked = true;
+      }
+    } else {
+      el.textContent = "reset";
+    }
+    centerPlace.insertAdjacentElement("beforeend", el);
+  }
+
+  config.forEach((item, index) => {
+    createElement(item);
+  });
+
+  document.addEventListener("click", (e) => {
+    let newPosition;
+    let newDairection;
+
+    const target = e.target;
+
+    if (target.classList.contains("reset")) {
+      window.location.reload(true);
+    }
+
+    if (
+      !target.closest("label") ||
+      target.closest("label").classList.contains("disabled")
+    )
+      return;
+
+    e.preventDefault();
+
+    if (target.firstElementChild.type === "checkbox") {
+      target.firstElementChild.checked = true;
+      if (target.classList.contains("active")) return;
+    }
+
+    const radios = Array.from(
+      document.querySelectorAll("input[type=checkbox]")
+    );
+    radios.forEach((item) => {
+      if (item.checked) {
+        newDairection = target.dataset.direction;
+      }
+    });
+
+    if (target.firstElementChild.type === "radio") {
+      const radios = Array.from(
+        document.querySelectorAll("input[type=checkbox]")
+      );
+      radios.map((item) => {
+        if (item.checked) {
+          newPosition = item.parentElement.dataset.position;
+        }
+      });
+      target.firstElementChild.checked = true;
+
+      runSpeedDial(newPosition, newDairection);
+      return;
+    }
+
+    Array.from(document.querySelectorAll(".active")).forEach((el) => {
+      el.classList.remove("active");
+      el.classList.remove("disabled");
+      el.firstElementChild.checked = false;
+    });
+
+    newPosition = target.dataset.position;
+    newDairection = target.dataset.direction;
+
+    config.forEach((item) => {
+      if (item.id === Number(target.dataset.id)) {
+        item.set.forEach((label, index) => {
+          const el = document.querySelector(`[data-id="${label}"]`);
+          el.classList.add("active");
+          el.classList.remove("disabled");
+
+          // console.log(index, label);
+          if (newPosition === "bottom-right" || newPosition === "bottom-left") {
+            if (index === 1) {
+              newPosition = target.dataset.position;
+              newDairection = el.dataset.direction;
+              el.firstElementChild.checked = true;
+            }
+          } else {
+            if (index === 2) {
+              newPosition = target.dataset.position;
+              newDairection = el.dataset.direction;
+              el.firstElementChild.checked = true;
+            }
+          }
+        });
+      }
+    });
+
+    Array.from(document.querySelectorAll("input[type=radio]")).forEach((el) => {
+      if (!el.parentNode.classList.contains("active")) {
+        el.parentNode.classList.add("disabled");
+      }
+    });
+
+    runSpeedDial(newPosition, newDairection);
+
+    function runSpeedDial(position, direction) {
+      const speedDialTop = document.querySelector(".speed-dial__top");
+      speedDialTop?.parentElement.removeChild(speedDialTop);
+
+      const btn =
+        position === "bottom-right" || position === "bottom-left"
+          ? true
+          : false;
+
+      const speedDial = document.querySelector(".speed-dial");
+      speedDial.parentElement.removeChild(speedDial);
 
       new SpeedDial({
         icons: speedIcons,
         steps: 50,
         position: 100,
+        modal: true,
+        visibility: true,
+        showTopBtn: btn,
         data: {
-          position: newPosition,
-          direction: newDairection,
+          position,
+          direction,
         },
       });
-    });
-  }
+    }
+  });
+
+  tippy("[data-tippy-content]");
 });
